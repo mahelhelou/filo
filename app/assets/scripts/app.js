6af0d081cbd4a1236200df09cc790557bdc9fa4c
mahelhelou <mahelhelou@gmail.com>
2022-03-11 20:32:00 +0200

    Adding some files for design

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,1 +1,49 @@
-// Main JS file
+jQuery(function ($) {
+	// let navbarHeight = $('.navbar').innerHeight()
+	let navbarHeight = $(".navbar").innerHeight();
+
+	// Adjust showcase height (100vh)
+	let windowHeight = $(window).height();
+	let windowWidth = $(window).width();
+
+	if (windowWidth < 1920) {
+		$(".showcase").height(windowHeight - navbarHeight);
+	}
+
+	// Smooth scroll
+	$(document).on("click", 'a[href^="#"]', function (event) {
+		event.preventDefault();
+
+		$("html, body").animate(
+			{
+				scrollTop: $($.attr(this, "href")).offset().top - navbarHeight,
+			},
+			500
+		);
+	});
+
+	// Testimonials owl carousel
+	$("#testimonials .owl-carousel").owlCarousel({
+		loop: true,
+		nav: true,
+		navText: [
+			"<i class='fa fa-chevron-left'></i>",
+			"<i class='fa fa-chevron-right'></i>",
+		],
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: true,
+		smartSpeed: 2000,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+			},
+			1000: {
+				items: 2,
+			},
+		},
+	});
+});
